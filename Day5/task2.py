f8f16541bade4074797ebedca2f21366c513c49d
FinnHuelsbusch <f.huelsbusch@outlook.de>
2023-12-06 14:29:42 +0100

    more work

--- Day5/task2.py
+++ Day5/task2.py
@@ -9,12 +9,10 @@ def get_lookup_table(mapping: str) -> list[tuple[range, int]]:
                 range(src_start, src_start + range_),
                 dest_start - src_start,
             ))
+        # Sort by source range start
+        table.sort(key=lambda x: x[0].start)
         return table
-def find_intersection(range1, range2):
-    return range(
-                        max(range1.start, range2.start),
-                        min(range1.stop, range2.stop),
-                    )
+  
 
 try: 
     with open("./Day5/input.txt") as f:
@@ -25,25 +23,29 @@ else:
    
     curr_data, *maps = file_content.split('\n\n')
     curr_data = [int(item) for item in curr_data.removeprefix('seeds: ').split()]
-    curr_data_range = []
+    curr_data_ranges = []
     for i in range(0, len(curr_data), 2):
-        curr_data_range.append(range(curr_data[i], curr_data[i]+ curr_data[i+1]))
+        curr_data_ranges.append(range(curr_data[i], curr_data[i]+ curr_data[i+1]))
     for mapping in maps:
-        temp_curr_data_range = []
+        temp_curr_data_ranges = []
+        # is zero in one of the ranges?
         table = get_lookup_table(mapping)
-        for item in curr_data_range:
-            for map_range, diff in enumerate(table):
-                # get the intersection of the two ranges
-                intersection = find_intersection(item, map_range)
+        for index, curr_data_range in enumerate(curr_data_ranges):
+            for map_range, diff in table:
+                intersection = range(max(map_range.start, curr_data_range.start),min(map_range.stop, curr_data_range.stop))
                 # if the intersection is not empty
                 if intersection:
                     # add the new range to the list
-                    temp_curr_data_range.append(range(min(intersection) + diff, max(intersection) + diff))
-                    # for the ones that are not in the intersection, add two ranges to the list
-                    temp_curr_data_range.append(range(item.start, min(intersection)))
-                    temp_curr_data_range.append(range(max(intersection), item.stop))
-                else:
-                    temp_curr_data_range.append(item)
-        curr_data_range = temp_curr_data_range
-    print( min(range_.start for range_ in curr_data_range))
+                    temp_curr_data_ranges.append(range(intersection.start + diff, intersection.stop + diff))
+                    if 0 in intersection:
+                        print("0 is in one of the ranges")
+                    # get range before intersection
+                    below_intersection = range(curr_data_range.start, intersection.start)
+                    above_intersection = range(intersection.stop, curr_data_range.stop)
+                    # update curr_data_ranges
+                    curr_data_ranges[index] = below_intersection
+                    curr_data_ranges.append(above_intersection)
+            
+        curr_data_ranges.extend(temp_curr_data_ranges)
+    print( min(range_.start for range_ in curr_data_ranges))
 
